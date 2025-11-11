993d7a2e75a2cfeb55fcf719bfa3661db886022d
Saidqodirxon Rahimov <rsaidqodirxon@gmail.com>
2025-11-12 00:17:49 +0500

    a

--- src/modules/subcategory/Subcategories.js
+++ src/modules/subcategory/Subcategories.js
@@ -1,1 +1,31 @@
-module.exports = require("../categories/Categories");
+const mongoose = require("mongoose");
+
+const CategoriesSchema = new mongoose.Schema(
+  {
+    name_uz: {
+      type: mongoose.SchemaTypes.String,
+      required: true,
+    },
+    name_ru: {
+      type: mongoose.SchemaTypes.String,
+      required: true,
+    },
+    name_en: {
+      type: mongoose.SchemaTypes.String,
+      required: true,
+    },
+    parentId: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "categories",
+      default: null,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: false,
+  }
+);
+
+const Categories = mongoose.model("subcategories", CategoriesSchema);
+
+module.exports = Categories;
